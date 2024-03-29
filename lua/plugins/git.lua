92d8416643c594605eb6983f4db79e5f032560d6
Frank Louwers <frank@louwers.be>
2024-03-29 12:01:13 +0100

    for some reason, opt{} is not working for neogit. Move to config

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -11,20 +11,22 @@ return {
       --    "ibhagwan/fzf-lua",              -- optional
     },
     cmd = 'Neogit',
-    config = true,
-    opt = {
-      use_per_project_settings = false,
-      graph_style = 'unicode',
-      git_services = {
-        ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
-        ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
-        ["gitlab.com"] =
-        "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
-        ["gitlab.x-plose.be"] =
-        "https://gitlab.x-plose.be/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
-      },
+    config = function()
+      local neogit = require('neogit')
+      neogit.setup({
+        use_per_project_settings = false,
+        graph_style = 'unicode',
+        git_services = {
+          ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+          ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
+          ["gitlab.com"] =
+          "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+          ["gitlab.x-plose.be"] =
+          "https://gitlab.x-plose.be/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+        },
+      })
+    end,
 
-    }
   },
 
   { -- gitsigns: git interactionas and indicate changes in sidebar
