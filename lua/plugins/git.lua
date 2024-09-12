a7a6767dee6f31c443b6d399ccf4d82b27f41eab
Frank Louwers <frank@louwers.be>
2024-09-12 16:50:12 +0200

    cleanup

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -16,14 +16,6 @@ return {
       neogit.setup({
         use_per_project_settings = false,
         graph_style = 'unicode',
-        git_services = {
-          ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
-          ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
-          ["gitlab.com"] =
-          "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
-          ["gitlab.x-plose.be"] =
-          "https://gitlab.x-plose.be/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
-        },
       })
     end,
 
