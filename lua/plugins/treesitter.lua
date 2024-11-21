17a3df998cfd4545e8ff9ea227b0822d83eb6567
Frank Louwers <frank@louwers.be>
2024-11-21 18:01:28 +0100

    move things around, move to fidget for notifications

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,15 @@
 -- parse files, provide syntax HL etc
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "lua", "javascript", "ruby", "python", "terraform" },
-      highlight = { enable = true },
-      indent = { enable = true }
-    })
-  end
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = { "lua", "javascript", "ruby", "python", "terraform" },
+        highlight = { enable = true },
+        indent = { enable = true }
+      })
+    end
+  },
 }
