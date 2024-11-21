f2e8e87cd80592f3a8e4bed401f104e9187c319b
Frank Louwers <frank@louwers.be>
2024-11-21 17:58:57 +0100

    delete old stuff + disable smoooooothing windows for now

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -18,33 +18,9 @@ return {
       vim.cmd.colorscheme "catppuccin-mocha"
     end,
   },
-  -- {
-  --   "marko-cerovac/material.nvim",
-  --   lazy = false,
-  --   name = "material",
-  --   priority = 1000,
-  --   config = function()
-  --     require("material").setup({
-  --       plugins = {
-  --         "gitsigns",
-  --         "mini",
-  --         "neogit",
-  --         "neo-tree",
-  --         "nvim-cmp",
-  --         "nvim-navic",
-  --         "nvim-web-devicons",
-  --         "telescope",
-  --         "trouble",
-  --         "which-key",
-  --         "nvim-notify",
-  --       },
-  --     })
-  --     vim.cmd.colorscheme "material-darker"
-  --   end,
-  -- },
   {
     "nvim-zh/colorful-winsep.nvim",
-    config = { smooth = false },
+    opts = { smooth = false },
     event = { "WinNew" },
   },
 }
