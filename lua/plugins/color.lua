dde85b4e5527ba698ede9acee3520ecccfc9b42e
Frank Louwers <frank@louwers.be>
2025-06-17 10:38:47 +0200

    change colorschame (again)

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,21 +1,33 @@
 return {
+  -- {
+  --   'rebelot/kanagawa.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("kanagawa").setup({
+  --       overrides = function(colors)
+  --         local theme = colors.theme
+  --         return {
+  --           Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+  --           PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+  --           PmenuSbar = { bg = theme.ui.bg_m1 },
+  --           PmenuThumb = { bg = theme.ui.bg_p2 },
+  --         }
+  --       end
+  --     })
+  --     vim.cmd.colorscheme "kanagawa"
+  --   end
+  -- },
   {
-    'rebelot/kanagawa.nvim',
-    lazy = false,
+    "romanaverin/charleston.nvim",
+    name = "charleston",
     priority = 1000,
     config = function()
-      require("kanagawa").setup({
-        overrides = function(colors)
-          local theme = colors.theme
-          return {
-            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-            PmenuSbar = { bg = theme.ui.bg_m1 },
-            PmenuThumb = { bg = theme.ui.bg_p2 },
-          }
-        end
+      require("charleston").setup({
+        italic = false,
+        darken_background = true
       })
-      vim.cmd.colorscheme "kanagawa"
+      vim.cmd.colorscheme "charleston"
     end
   },
 
